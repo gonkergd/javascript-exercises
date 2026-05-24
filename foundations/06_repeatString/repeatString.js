1e2710f5494720c6b50a14ed50e845ce347acdb6
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-05-25 01:05:15 +0800

    Finish 06
    i guess bro

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function repeatString(string, num) {
+    if (num < 0) return "ERROR";
+    let next = "";
+    for (let i = 0; i < num; i++){
+        next += string;
+    }
+    return next;
 };
 
 // Do not edit below this line
