6e0de286fcdead983d0f9768fb8ee9c8ff67443e
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-05-25 01:21:43 +0800

    finish 08
    wow i actually learned something new! no way!

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    array = arguments[0];
+    for (let i = 1; i < arguments.length; i++){
+        num = arguments[i];
+        while (array.includes(num)){
+            array.splice(array.indexOf(num), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
