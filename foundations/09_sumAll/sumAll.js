3c974982326c2e5679cc5da099b1aebe4e930898
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-05-25 01:31:09 +0800

    finished 09
    I learned something again! typeof and detecting integers w/js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != 'number' || typeof b != 'number' 
+        || a % 1 != 0 || b % 1 != 0
+        || a < 0 || b < 0) return "ERROR";
+    let hi = 0;
+    if (a < b){
+        for (let i = a; i <= b; i++){
+            hi += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++){
+            hi += i;
+        }
+    }
+    return hi;
 };
 
 // Do not edit below this line
