dac09594ac39ca63e78b3df43c23a5c9b0e5eecc
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-05-27 11:58:17 +0800

    Finish 13
    i stole the regex im not implementing this shit myself fuck you

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let b = a.replace(/[^a-zA-Z0-9]/g, "");
+    return b == b.split('').reverse().join('');
 };
 
 // Do not edit below this line
