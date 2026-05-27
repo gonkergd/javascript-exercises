3782385e72e6b15be79c9cd2cd4d621ffb4bece0
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-05-27 12:43:33 +0800

    Finish 16 and foundations
    js is so unpredictable i hate it

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let newPeople = people.map(n => {
+        if (n.yearOfDeath == undefined) return 2026 - n.yearOfBirth;
+        return n.yearOfDeath - n.yearOfBirth;
+    });
+    return people[newPeople.findIndex(n => n == Math.max(...newPeople))];
 };
-
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
